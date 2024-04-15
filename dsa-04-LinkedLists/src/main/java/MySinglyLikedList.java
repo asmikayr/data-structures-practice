73a446f7ebcf78b3dcdad6f1ac6be1cb47474694
Asmik <jasmin_ayr@mail.ru>
2024-04-15 17:02:25 +0200

    LinkedList delete, add, indexOf methods added

--- dsa-04-LinkedLists/src/main/java/MySinglyLikedList.java
+++ dsa-04-LinkedLists/src/main/java/MySinglyLikedList.java
@@ -20,13 +20,79 @@ public class MySinglyLikedList {
         }
     }
 
+    void addFirst(int data){
+        //create a new node object from data
+        Node node =  new Node(data);
+        if(isEmpty()){
+            head=tail=node;
+        }else{
+            node.next=head;
+            head=node;
+        }
+    }
+
+
+    int indexOf(int id){
+        if(isEmpty()) return -1;
+        int pos=0;
+        //iterate through the list
+        Node current=head;//set my current with the starting element;
+        while(current!= null){
+            if(current.id==id) return pos;
+            pos++;
+            current=current.next;
+
+        }
+        return -1;
+    }
+
+    void deleteById(int id){
+        //check if empty
+        if(isEmpty()) System.out.println("List is empty!");
+        //assign prev and current with the head
+        Node prev=head;
+        Node current=head;
+
+        while (current != null) {
+            if(current.id==id){//there is a match
+                //case1: head
+                if(current==head){
+                    head=current.next;
+                    current.next=null;
+
+              //case2: tail
+                }else if(current==tail){
+
+                    tail=prev;
+                    prev.next=null;
+                //case3: middle
+                }else{
+                    prev.next=current.next;
+                    current.next=null;
+                }
+
+
+                //after deletion
+
+                size--;
+            }
+            //move forward on the elements of the list
+            prev = current;
+            current = current.next;
+        }
+    }
+
     void printNodes(){
         Node current=head;
         while(current!=null){
             if(current.next== null) System.out.println(current.id+"=>null");
-            System.out.println(current.id+"=>");
+            else{
+                System.out.print(current.id+"=>");
+            }
             current=current.next;
+
         }
     }
+
 
 }
--- dsa-04-LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ dsa-04-LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -3,5 +3,16 @@ public class SinglyLinkedListApp {
         MySinglyLikedList myList = new MySinglyLikedList();
         System.out.println(myList.isEmpty());
 
+        for (int i = 0; i < 10; i++) {
+            myList.add(i);
+        }
+
+        myList.printNodes();
+        myList.deleteById(8);
+        myList.printNodes();
+        myList.addFirst(50);
+        myList.printNodes();
+        System.out.println(myList.indexOf(1));
+
     }
 }
