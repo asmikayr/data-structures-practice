fbc5360790e5ad4ff81eef7e80d9b362ed257a24
Asmik <jasmin_ayr@mail.ru>
2024-05-26 19:14:10 +0200

    MyHashMap is done

---
+++ dsa-07-Queue/src/main/java/myHashMap/BucketLinkedList.java
@@ -0,0 +1,43 @@
+package myHashMap;
+
+import java.util.LinkedList;
+
+public class BucketLinkedList {
+    LinkedList<KeyValue<Integer, Integer>> bucketList;
+
+    public BucketLinkedList() {
+        this.bucketList = new LinkedList<KeyValue<Integer, Integer>>();
+    }
+
+    public int get(int key){
+
+        for(KeyValue<Integer, Integer> pair: bucketList){
+            if(pair.key.equals(key)) return pair.value;
+        }
+        return -1;
+    }
+
+    public void put (int key, int value){
+        //if there is match update value
+        for(KeyValue<Integer, Integer> pair: bucketList){
+            if(pair.key.equals(key))  {pair.value=value;
+                return;}
+
+        }
+        //if no match add it
+        this.bucketList.add(new KeyValue<>(key, value));
+    }
+
+    public void remove(int key){
+
+        for(KeyValue<Integer, Integer> pair: bucketList){
+            if(pair.key.equals(key))  {
+                //if there is a match remove that pair
+                this.bucketList.remove(pair);
+                return;
+            }
+
+        }
+
+    }
+}
---
+++ dsa-07-Queue/src/main/java/myHashMap/MyHashMap.java
@@ -0,0 +1,50 @@
+package myHashMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyHashMap {
+
+        List<BucketLinkedList> list;
+
+        public int modNumber;
+
+        public MyHashMap(){
+            this.modNumber = 2003;
+            this.list=new ArrayList<BucketLinkedList>();
+            for (int i = 0; i < 2003; i++) {
+                this.list.add(new BucketLinkedList());
+
+            }
+        }
+
+
+        public void put(int key, int value) {
+            //calculate index from the key with modulus
+            int index = key% modNumber;
+            list.get(index).put(key, value);
+
+        }
+
+        public int get(int key) {
+            //calculate index from the key with modulus
+            int index=key % modNumber;
+            return list.get(index).get(key);
+
+        }
+
+        public void remove(int key) {
+            int index= key%modNumber;
+            list.get(index).remove(key);
+
+        }
+    }
+
+/**
+ * Your myHashMap.MyHashMap object will be instantiated and called as such:
+ * myHashMap.MyHashMap obj = new myHashMap.MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
