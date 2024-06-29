53cf4112137f3bc22c4a5c4842295fea2b063b6f
Asmik <jasmin_ayr@mail.ru>
2024-06-29 19:52:06 +0200

    D09 - Heap - MyHeap is created, pom set up

---
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -0,0 +1,18 @@
+public class MyHeap {
+    int[] items;
+    int size;
+
+    public MyHeap(int capacity){
+        this.items = new int[capacity];
+        this.size = 0;
+    }
+
+    public int leftChildIndex(int index){return index*2+1;}
+    public int rightChildIndex(int index){return index*2+2;}
+    public int parentChildIndex(int index){return (index-1)/2;}
+
+
+
+
+
+}
