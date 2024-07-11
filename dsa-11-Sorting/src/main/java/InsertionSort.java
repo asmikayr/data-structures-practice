95b58f77e8cdb06d6ee86c085e9c55d77be03347
Asmik <jasmin_ayr@mail.ru>
2024-07-11 11:29:30 +0200

    D11 Sorting - InsertionSort done

---
+++ dsa-11-Sorting/src/main/java/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public static int[] insertionSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+            int toBeInserted=array[i];//temp var
+            int j = i-1;
+            while(j>=0&&array[j]>toBeInserted){
+                array[j+1]=array[j];
+                j--;
+            }
+            array[j+1]=toBeInserted;
+        }
+        return array;
+    }
+}
